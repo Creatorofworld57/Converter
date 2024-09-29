a275614cf968e3be305c91afd1c4fa6effd3e2db
CreatorOfWorld57 <dimu.29077@gmail.com>
2024-09-29 23:35:32 +0300

    Front

---
+++ .Frontend/spa/src/PageForRender.jsx
@@ -0,0 +1,21 @@
+
+import React, {useContext, useEffect, useState} from 'react'
+import './Styles/Menu.css'
+const PageForRender= ({activity}) => {
+
+
+
+
+
+
+
+
+    return (
+        <div className={activity?'menu active':'menu'}>
+
+
+        </div>)
+
+
+}
+export default PageForRender;
